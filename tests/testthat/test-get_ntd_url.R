f51b13de3eb0672253eaf5286010229686680600
vgXhc <kliems@gmail.com>
2023-02-25 11:02:53 -0600

    created some tests; removed example file

---
+++ tests/testthat/test-get_ntd_url.R
@@ -0,0 +1,16 @@
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("returns a URL that starts right", {
+  expect_match(get_ntd_url(), regexp = "^/sites/fta.dot.gov/files/")
+})
+
+test_that("URL contains the right terms for different data_types", {
+  expect_match(get_ntd_url("raw"), regexp = "Raw")
+  expect_match(get_ntd_url("adjusted"), regexp = "Adjusted")
+})
+
+test_that("function returns error for invalid parameter values", {
+          expect_error(get_ntd_url("nonsense"))
+  })
